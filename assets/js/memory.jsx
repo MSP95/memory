4ac3b80063c88b6ba4787325678dfb06830fb42b
MSP95 <patilmanish1910@gmail.com>
2018-01-27 01:08:51 -0500

    added winnerFunction

--- assets/js/memory.jsx
+++ assets/js/memory.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import classnames from 'classnames';
 
 export default function memory_init(root) {
   ReactDOM.render(<Board />, root);
@@ -13,9 +14,14 @@ class Square extends React.Component {
       this.props.action(this.props.id, this.props.value, this.props.flag,score);
     }
   }
+
   render() {
+    let btnClass = classnames({
+      'square-tile': true,
+      'square-selected': this.props.current != null,
+    });
     return (
-      <button id={this.props.id} className="squarew" onClick={()=>this.change()}>
+      <button id={this.props.id} className={btnClass} onClick={()=>this.change()}>
         {this.props.current}
       </button>
     );
@@ -37,7 +43,7 @@ class Board extends React.Component {
       isPaused: false,
     };
   }
-  randomfunc(id,curr, val,flag, score){
+  changeState(id,curr, val,flag, score){
     let prev = {id: id,value: val};
     this.setState({
       previous: prev,
@@ -84,88 +90,99 @@ class Board extends React.Component {
       flag = !flagy;
       // Its second tile
       if(flag == true){
-        // elements match
+        // elements match <div>&#10003;</div>;
         if(previousVal == val){
           this.pauseInput(true);
-          setTimeout(() => {currentValues[id] = <div>&#10003;</div>;
-            currentValues[previousId]=<div>&#10003;</div>;
-              this.randomfunc(id, currentValues,val,flag, score);
-              this.pauseInput(false)
-            }, 200);
-          }
-          //elements dont match
-          else{
-            this.pauseInput(true);
-            setTimeout(() => {currentValues[id] = null;
-              currentValues[previousId]=null;
-              this.randomfunc(id, currentValues,val,flag, score);
+          this.pauseInput(true);
+          setTimeout(() => {currentValues[id] = <div>&#10003;</div>;;
+            currentValues[previousId]=<div>&#10003;</div>;;
+              this.changeState(id, currentValues,val,flag, score);
               this.pauseInput(false);
-            }, 1000);
-          }
+            }, 200
+          );
         }
-        // its first tile
-        else {
-          currentValues[id] = val;
+        //elements dont match
+        else{
+          this.pauseInput(true);
+          setTimeout(() => {currentValues[id] = null;
+            currentValues[previousId]=null;
+            this.changeState(id, currentValues,val,flag, score);
+            this.pauseInput(false);
+          }, 1000);
         }
-        this.randomfunc(id, currentValues,val,flag, score);
       }
-      // retired element clicked
-      else{}
+      // its first tile
+      else {
+        currentValues[id] = val;
+      }
+      this.changeState(id, currentValues,val,flag, score);
     }
+    // retired trueelement clicked
 
-
-    renderSquare(props) {
-      return (
-        <Square
-          id = {props}
-          value={this.state.squares[props]}
-          current={this.state.currentValues[props]}
-          action = {this.changeHandler}
-          previous = {this.state.previous}
-          flag={this.state.flag}
-          score={this.state.score}
-          pause={this.state.isPaused}/>
-      );
-    }
-    render() {
-      return (
-        <div className="main-game">
-          <div className="status"><h2>Score: {this.state.score}</h2>
-          <button
-            type = "button"
-            className="reset-button"
-            onClick={()=>this.reset()}>Reset</button></div>
-          <div className="tile-grid">
-            <table className="table-grid">
-              <tbody>
-                <tr>
-                  <td>{this.renderSquare(0)}</td>
-                  <td>{this.renderSquare(1)}</td>
-                  <td>{this.renderSquare(2)}</td>
-                  <td>{this.renderSquare(3)}</td>
-                </tr>
-                <tr>
-                  <td>{this.renderSquare(4)}</td>
-                  <td>{this.renderSquare(5)}</td>
-                  <td>{this.renderSquare(6)}</td>
-                  <td>{this.renderSquare(7)}</td>
-                </tr>
-                <tr>
-                  <td>{this.renderSquare(8)}</td>
-                  <td>{this.renderSquare(9)}</td>
-                  <td>{this.renderSquare(10)}</td>
-                  <td>{this.renderSquare(11)}</td>
-                </tr>
-                <tr>
-                  <td>{this.renderSquare(12)}</td>
-                  <td>{this.renderSquare(13)}</td>
-                  <td>{this.renderSquare(14)}</td>
-                  <td>{this.renderSquare(15)}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      );
+  }
+  calculateWinner(){
+    if(_.every(this.state.currentValues,function(num){
+      return num != null
+    })){
+      return <div><h1> Congrats! You are a winner!</h1>
+      <h4><i>Click Reset button to play again.</i></h4></div>
     }
   }
+
+  renderSquare(props) {
+    return (
+      <Square
+        id = {props}
+        value={this.state.squares[props]}
+        current={this.state.currentValues[props]}
+        action = {this.changeHandler}
+        previous = {this.state.previous}
+        flag={this.state.flag}
+        score={this.state.score}
+        pause={this.state.isPaused}
+        />
+    );
+  }
+  render(){
+    return (
+      <div className="main-game">
+        <div className="status"><h2>Score: {this.state.score}</h2>
+        <div className="winner">{this.calculateWinner()}</div>
+        <button
+          type = "button"
+          className="reset-button"
+          onClick={()=>this.reset()}>Reset</button></div>
+        <div className="tile-grid">
+          <table className="table-grid">
+            <tbody>
+              <tr>
+                <td>{this.renderSquare(0)}</td>
+                <td>{this.renderSquare(1)}</td>
+                <td>{this.renderSquare(2)}</td>
+                <td>{this.renderSquare(3)}</td>
+              </tr>
+              <tr>
+                <td>{this.renderSquare(4)}</td>
+                <td>{this.renderSquare(5)}</td>
+                <td>{this.renderSquare(6)}</td>
+                <td>{this.renderSquare(7)}</td>
+              </tr>
+              <tr>
+                <td>{this.renderSquare(8)}</td>
+                <td>{this.renderSquare(9)}</td>
+                <td>{this.renderSquare(10)}</td>
+                <td>{this.renderSquare(11)}</td>
+              </tr>
+              <tr>
+                <td>{this.renderSquare(12)}</td>
+                <td>{this.renderSquare(13)}</td>
+                <td>{this.renderSquare(14)}</td>
+                <td>{this.renderSquare(15)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
